01b56a13c8d72e1e2fc4651318dc0833aeff0603
Thierry Goubier <thierry.goubier@gmail.com>
2017-03-10 19:00:45 +0100

    tests scripts for integration

---
+++ resources/scripts/failIfNotSet.sh
@@ -0,0 +1,10 @@
+
+(cd testMergeDriver; git checkout branch2; git merge branch1)
+if [ $? -eq 0 ]; then
+	echo "git merge should have failed"
+	exit_code=1
+else
+	exit_code=0
+fi
+rm -rf testMergeDriver
+exit $exit_code
---
+++ resources/scripts/tests.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}")" && pwd )"
+echo $DIR
+cd $DIR
+mkdir ../test
+tests_status=0
+(cd ../test; pwd; ../scripts/setup.sh; ../scripts/failIfNotSet.sh)
+if [ $? -ne 0 ]; then
+	tests_status=1
+fi
+(cd ../test; ../scripts/setup.sh; ../scripts/failIfNotSet.sh)
+if [ $? -ne 0 ]; then
+	tests_status=1
+fi
+exit $tests_status
